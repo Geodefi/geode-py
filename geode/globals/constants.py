87a2d88d3f38707f483d3744184f7e2ed6d77831
0xCypherr <cypher@geode.fi>
2023-09-25 14:59:46 +0200

    FEAT: Added keystore dictionary list

--- geode/globals/constants.py
+++ geode/globals/constants.py
@@ -10,13 +10,11 @@ ETH2GWEI = 10 ** 9
 DENOMINATOR = 10 ** 10
 
 DEPOSIT_CLI_VERSION = '2.4.0'
-
 WORD_LIST_PATH = os.path.join('geode', 'utils', 'bls')
 
 
 # Execution-spec constants taken from https://github.com/ethereum/consensus-specs/blob/dev/specs/phase0/beacon-chain.md
 DOMAIN_DEPOSIT = bytes.fromhex('03000000')
-BLS_WITHDRAWAL_PREFIX = bytes.fromhex('00')
 ETH1_ADDRESS_WITHDRAWAL_PREFIX = bytes.fromhex('01')
 
 MIN_DEPOSIT_AMOUNT = 2 ** 0 * ETH
